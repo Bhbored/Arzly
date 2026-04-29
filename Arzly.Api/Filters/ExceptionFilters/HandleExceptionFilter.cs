cb86e2ea3efaa29b95ef2e1b20f03a3b60793c3d
Bhbored <bhbored2022@gmail.com>
2026-04-29 14:07:24 +0300

    added expection filter

---
+++ Arzly.Api/Filters/ExceptionFilters/HandleExceptionFilter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Arzly.Api.Filters.ExceptionFilters
+{
+    public class HandleExceptionFilter : IAsyncExceptionFilter
+    {
+        private readonly ILogger<HandleExceptionFilter> _logger;
+        public HandleExceptionFilter(ILogger<HandleExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+
+            _logger.LogInformation("{Filter}.{Method} - Before",
+                GetType().Name, nameof(OnExceptionAsync));
+            var env = context.HttpContext.RequestServices.GetService<IHostEnvironment>();
+
+            _logger.LogError("Exception: {ExceptionType} - {Message}",
+                context.Exception.GetType().Name,
+                context.Exception.Message);
+
+            var statusCode = context.Exception switch
+            {
+                ArgumentException or ArgumentNullException or FormatException => 400,
+                UnauthorizedAccessException => 401,
+                KeyNotFoundException or FileNotFoundException => 404,
+                InvalidOperationException => 400,
+                TimeoutException or OperationCanceledException => 408,
+                NotImplementedException => 501,
+                _ => 500
+            };
+            //might change when hosting
+            var response = new
+            {
+                success = false,
+                message = env?.IsDevelopment() == true
+                    ? context.Exception.Message
+                    : "An error occurred"
+            };
+
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = statusCode;
+
+            context.Result = new ObjectResult(response)
+            {
+                StatusCode = statusCode
+            };
+            _logger.LogInformation("{Filter}.{Method} - After",
+               GetType().Name, nameof(OnExceptionAsync));
+            context.ExceptionHandled = true;//no short-circuit
+            await Task.CompletedTask;
+        }
+
+    }
+}
--- Arzly.Api/Program.cs
+++ Arzly.Api/Program.cs
@@ -1,7 +1,9 @@
 using Arzly.Api.Application.Services;
+using Arzly.Api.Filters.ExceptionFilters;
 using Arzly.Api.Infrastructure.Data.DataBaseContext;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,10 +25,14 @@ builder.Services.RegisterDependencies(builder.Configuration);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
+builder.Services.AddControllers(controller =>
+{
+    controller.Filters.Add<HandleExceptionFilter>();//global filter
+
+})
     .ConfigureApiBehaviorOptions(options =>
     {
-        options.SuppressModelStateInvalidFilter = true;
+        options.SuppressModelStateInvalidFilter = true;//for built-in modelBinding i did a custom filter for that
     });
 builder.Services.AddOpenApi();
 
@@ -34,6 +40,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSerilogRequestLogging();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
